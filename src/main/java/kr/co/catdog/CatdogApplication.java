4fadbedf33c5ef1a2ed34ef0e8ae42e67a674f88
김종식 <otw1917@naver.com>
2023-03-08 02:54:49 +0900

    start project

---
+++ src/main/java/kr/co/catdog/CatdogApplication.java
@@ -0,0 +1,15 @@
+package kr.co.catdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatdogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatdogApplication.class, args);
+
+        System.out.println("hello world!");
+    }
+
+}
