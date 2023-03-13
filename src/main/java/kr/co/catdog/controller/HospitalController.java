a3f0d5ac3d4071626f5780b22bf2c928e98e2bba
JunGi Jeong <59526539+devjun63@users.noreply.github.com>
2023-03-13 11:51:15 +0900

    Jungi (#7)
    * readme 수정
    
    * kakao map api with geoloaction
    
    * add packages

--- src/main/java/kr/co/catdog/controller/HospitalController.java
+++ src/main/java/kr/co/catdog/controller/HospitalController.java
@@ -10,6 +10,4 @@ public class HospitalController {
     public String hospital() {
         return "hospital";
     }
-
-
 }
---
+++ src/main/java/kr/co/catdog/service/HospitalService.java
@@ -0,0 +1,4 @@
+package kr.co.catdog.service;
+
+public interface HospitalService {
+}
