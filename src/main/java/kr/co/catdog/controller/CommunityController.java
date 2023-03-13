15828377a212cefd4fcb060e33b9b1160f3ec121
oster <kms0843@naver.com>
2023-03-13 15:49:35 +0900

    commit

--- src/main/java/kr/co/catdog/controller/CommunityController.java
+++ src/main/java/kr/co/catdog/controller/CommunityController.java
@@ -3,14 +3,17 @@ package kr.co.catdog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("user/community")
 public class CommunityController {
 	
 	@GetMapping("list")
-	String list() {
-		return "user/community/list-community";
+	ModelAndView list() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("user/community/list-community");
+		return mav;
 	}
 
 }
