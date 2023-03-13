603bab34e2fb92bb985c2ef981ed8159d5292a84
JunGi Jeong <59526539+devjun63@users.noreply.github.com>
2023-03-13 11:38:48 +0900

    Jungi (#6)
    * readme 수정
    
    * kakao map api with geoloaction

---
+++ src/main/java/kr/co/catdog/controller/HospitalController.java
@@ -0,0 +1,15 @@
+package kr.co.catdog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HospitalController {
+
+    @GetMapping("/hospital")
+    public String hospital() {
+        return "hospital";
+    }
+
+
+}
