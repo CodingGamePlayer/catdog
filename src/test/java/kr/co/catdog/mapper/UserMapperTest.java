00649236c3d8fbbd46410dc82751c5bab00f6ab0
kohyuny <hahahahaha@hahah.a>
2023-03-15 18:01:50 +0900

    user

--- src/test/java/kr/co/catdog/mapper/UserMapperTest.java
+++ src/test/java/kr/co/catdog/mapper/UserMapperTest.java
@@ -6,21 +6,29 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
 @Slf4j
+@SpringBootTest
+
 class UserMapperTest {
 @Autowired
 UserMapper userMapper;
     @Test
     void selectOne() {
+        UserVO userVO = UserVO.builder()
+                .user_id("asdf")
+                .user_name("킬러").build();
+
+        System.out.println(userMapper.selectOne(userVO));
     }
 
     @Test
+    @Transactional
     void insert() {
         UserVO userVO = UserVO.builder()
-                .user_id("asdf")
+                .user_id("fdas")
                 .user_pw("1233")
                 .user_name("지현")
                 .user_image("asdf")
@@ -28,15 +36,26 @@ UserMapper userMapper;
                 .user_phoneNumber("012121515")
                 .build();
 
-        userMapper.insert(userVO);
+
+        Assertions.assertThat(userMapper.insert(userVO)).isEqualTo(0);
+
 
     }
 
     @Test
     void update() {
+        UserVO userVO = UserVO.builder()
+                .user_id("asdf")
+                .user_pw("123")
+                .user_name("킬러").build();
+        userMapper.update(userVO);
+//        Assertions.assertThat(userMapper.update(userVO)).isEqualTo(1);
     }
 
     @Test
     void delete() {
+        UserVO userVO = UserVO.builder()
+                .user_id("asdf").build();
+        userMapper.delete(userVO);
     }
 }
