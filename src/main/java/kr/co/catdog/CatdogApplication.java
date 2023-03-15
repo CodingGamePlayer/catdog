9e2692f63519751fcd01ff61af76eb5abe334edf
devjun <devjun63@gmail.com>
2023-03-15 11:17:19 +0900

    make kakaoAPI coord2regioncode example

--- src/main/java/kr/co/catdog/CatdogApplication.java
+++ src/main/java/kr/co/catdog/CatdogApplication.java
@@ -1,14 +1,20 @@
 package kr.co.catdog;
 
+import org.json.simple.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class CatdogApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CatdogApplication.class, args);
-        System.out.println("hello world!");
     }
 
 }
