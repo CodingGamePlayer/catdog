1a8de77d131ca45c456cdda47e59edd06380220b
지현 <124012755+kohyuny@users.noreply.github.com>
2023-03-16 18:05:08 +0900

    Koko (#15)
    * navbar commit
    
    * user
    
    * user2
    
    * commit
    
    * commit
    
    * community-detail
    
    * commit
    
    * commit
    shop
    
    * commit
    user
    
    * usermapper
    
    * usermapper
    
    * user
    
    * user session
    login
    
    ---------
    
    Co-authored-by: kohyuny <hahahahaha@hahah.a>
    Co-authored-by: koko <>

--- src/main/java/kr/co/catdog/controller/AccountController.java
+++ src/main/java/kr/co/catdog/controller/AccountController.java
@@ -1,16 +1,67 @@
 package kr.co.catdog.controller;
 
+import kr.co.catdog.dto.UserDTO;
+import kr.co.catdog.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Controller
+@Slf4j
 public class AccountController {
+    private UserService userService;
+
+    public AccountController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/login")
-    String login() {
+    String signinForm() {
         return "sign-in";
     }
+
+    @PostMapping("/login")
+    String signin(UserDTO userDTO, HttpServletRequest request) {
+        UserDTO user = userService.findById(userDTO.getUser_id());
+//        id없을때 정보없다고알려줘
+
+        if (user.getUser_pw().equals(userDTO.getUser_pw())) {
+            HttpSession session = request.getSession();
+            session.setAttribute("loginInfo", userDTO.getUser_id());
+
+            log.info(String.valueOf(session.getAttribute("loginInfo")));
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping("/logout")
+    String logout(HttpServletRequest request) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+
+        if(session!=null){
+            session.invalidate();
+        }
+
+        return "redirect:/";
+    }
+
     @GetMapping("/signup")
-    String signup(){
+    String signupForm() {
         return "sign-up";
     }
+
+    @PostMapping("/signup")
+    String signup(UserDTO userDTO) {
+        int result = userService.insert(userDTO);
+        log.info(String.valueOf(result));
+        return "redirect:/login";
+    }
+
+
 }
---
+++ src/test/java/kr/co/catdog/service/impl/UserServiceImpTest.java
@@ -0,0 +1,45 @@
+package kr.co.catdog.service.impl;
+
+import kr.co.catdog.domain.UserVO;
+import kr.co.catdog.dto.UserDTO;
+import kr.co.catdog.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+@Slf4j
+class UserServiceImpTest {
+    @Autowired
+    ModelMapper modelMapper;
+    @Autowired
+    UserMapper userMapper;
+
+    @Test
+    void selectOne() {
+        UserDTO userDTO = UserDTO.builder()
+                .user_id("1")
+                .build();
+        UserVO userVO = userMapper.selectOne(modelMapper.map(userDTO, UserVO.class));
+        UserDTO userDTO1 = modelMapper.map(userVO, UserDTO.class);
+        log.info(String.valueOf(userVO));
+        log.info(String.valueOf(userDTO1));
+
+    }
+
+    @Test
+    void insert() {
+
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
