e6e4ff77a467ab3fe49732233ddd109d136dbaa7
devjun <devjun63@gmail.com>
2023-04-04 15:12:12 +0900

    check environment key with cloudtype

--- src/main/java/kr/co/catdog/apicontroller/CommunityApiController.java
+++ src/main/java/kr/co/catdog/apicontroller/CommunityApiController.java
@@ -11,17 +11,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import kr.co.catdog.domain.ReplyVO;
-import kr.co.catdog.dto.ReplyDTO;
+
 import kr.co.catdog.service.CommunityService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -64,6 +62,7 @@ public class CommunityApiController {
 				.build();
 		return communityService.selectSize(communityDTO);
 	}
+
 
 
 
