f0ebc6cd6730173a183542909793c0320bdebaf4
nickoster <120629848+nickoster@users.noreply.github.com>
2023-04-05 15:38:08 +0900

    Minsu (#79)
    * url 경로 수정
    
    * index
    
    * commit
    
    * community 중간 커밋
    register 개발중
    
    * modelmapper dependency 추가
    application.properties에
    mybatis mapper 경로 설정
    community테이블 db저장까지 완료
    
    * 등록된 커뮤니티 글 list에 담아오는 구현 중
    중간 커밋
    
    * img 불러오기 위해
    RestController 생성
    
    * pull위한 commit
    
    * 댓글 fetch이용해서 구현중 중간 커밋
    
    * 커뮤니티 오류 잡음
    
    * community reply register, select List추가
    
    * community controller register
    loginInfo -> session_id
    
    * 주석
    
    * communityController
    file upload value 경로 변경
    
    * community 수정 구현 중
    
    * like 기능 추가(미완성)
    
    * 좋아요 수정 중 commit
    좋아요 기능은 구현 완료
    css 변경중
    
    * 좋아요 기능, css 구현 완료
    
    글쓰기 디자인 완료
    
    * 내가 쓴 글 구현 commit
    
    * 내가 쓴 글, 내가쓴 댓글 구현
    기본 이미지 변경
    
    * 수정 삭제 아이콘으로 변경
    
    * 인기글 경로 설정
    삭제 아이콘 추가
    
    * like 테이블 -> heart 테이블 수정 후
    community list , 좋아요, 댓글 수정
    
    * 주석 커밋
    
    * 매칭 뷰, controller, service, dto 구현
    
    * 머지 위한 중간 커밋
    
    * matching 구현중 머지 위한 중간 커밋
    
    * matching 구현중 머지 위한 중간 커밋
    
    * matching 탭 구현 중 중간 커밋
    
    * matching 기능 구현 1차 완료
    
    * matching 에러 수정 완료
    
    * edit-person matchinguse param null issue fixed
    
    * community 특정 위치에 스크롤 도달시
    추가 글 load 구현 완료
    
    * community 수정, 삭제 버튼 오류 수정
    
    * 주석 커밋
    
    * 인기글, 내가쓴글, 커뮤니티 동적쿼리 사용 구현
    
    * 이미지 경로 프로젝트 내 폴더로 변경
    이미지 찾기 실패시 에러 발생 현상
    controllerAdvice 생성

--- src/main/java/kr/co/catdog/service/CommunityService.java
+++ src/main/java/kr/co/catdog/service/CommunityService.java
@@ -1,28 +1,32 @@
 package kr.co.catdog.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import kr.co.catdog.domain.CommunityVO;
 import kr.co.catdog.domain.ReplyVO;
 import kr.co.catdog.dto.CommunityDTO;
 import kr.co.catdog.dto.ReplyDTO;
+import org.springframework.core.io.Resource;
 
 public interface CommunityService {
 	
-	List<CommunityVO> selectAll(CommunityVO communityVO);
+//	List<CommunityVO> selectAll(CommunityVO communityVO);
 	
-	int register(CommunityDTO communityDTO);
+	int register(CommunityDTO communityDTO) throws IOException;
 	
 	CommunityDTO findByCommunity(CommunityDTO communityDTO);
 
-	int update(CommunityDTO communityDTO);
+	int update(CommunityDTO communityDTO) throws IOException;
 	
 	int delete(CommunityDTO communityDTO);
 	
-	List<CommunityVO> myCommunity(CommunityDTO communityDTO);
+//	List<CommunityVO> myCommunity(CommunityDTO communityDTO);
 	
-	List<CommunityVO> popularPosts(CommunityDTO communityDTO);
+//	List<CommunityVO> popularPosts(CommunityDTO communityDTO);
 
 	List<CommunityDTO> selectSize(CommunityDTO communityDTO);
+
+	Resource getMedia(String filename);
 
 }
